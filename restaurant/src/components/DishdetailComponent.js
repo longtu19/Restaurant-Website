7bdef9ad93b7149869b9bcfae8ddd2e753f208cd
Long Huynh <longbhuynh019@gmail.com>
2022-04-04 04:04:31 -0400

    redux thunk

--- restaurant/src/components/DishdetailComponent.js
+++ restaurant/src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 Form, FormGroup, Input, Label, Row, Col} from 'reactstrap'
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors } from 'react-redux-form'
+import {Loading } from './LoadingComponent'
 
 
 
@@ -167,7 +168,26 @@ class RenderSubmitComments extends Component {
 
 
 const DishDetail = (props) => {
-    if (props.dish != null){
+    if (props.isLoading){
+        return (
+            <div className = "container">
+                <div className = "row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }
+    else if (props.errMess){
+        return (
+            <div className = "container">
+                <div className = "row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        )
+
+    }
+    else if (props.dish != null){
         return (
             <div className = "container">
                     <div className = "row">

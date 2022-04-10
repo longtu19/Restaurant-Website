359b15121d829d967cb75ec250437aa37ddd380c
Long Huynh <longbhuynh019@gmail.com>
2022-04-10 02:28:20 -0400

    fetch handling errors

--- restaurant/src/Redux/ActionCreators.js
+++ restaurant/src/Redux/ActionCreators.js
@@ -18,8 +18,23 @@ export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true))
 
     return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok){
+                return response;
+            }
+            var error = new Error('Error ' + response.status + ': ' + response.statusText)
+            error.response = response
+            throw error
+        },
+        
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess
+
+        })
         .then(response => response.json())
         .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)))
 
 
 }
@@ -42,8 +57,24 @@ export const addDishes = (dishes) => ({
 export const fetchComments = () => (dispatch) => {
 
     return fetch(baseUrl + 'comments')
+        .then(response => {
+                if (response.ok){
+                    return response;
+                }
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response
+                throw error
+            },
+            
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess
+
+            })
         .then(response => response.json())
         .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)))
+
 
 
 
@@ -66,8 +97,24 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true))
 
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if (response.ok){
+                return response;
+            }
+            var error = new Error('Error ' + response.status + ': ' + response.statusText)
+            error.response = response
+            throw error
+        },
+        
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess
+
+        })
         .then(response => response.json())
         .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)))
+
 
 
 }

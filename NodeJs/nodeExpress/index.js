3eaa10a847acb1d4da8cd2fe7283ab951802e7bd
Long Huynh <longbhuynh019@gmail.com>
2022-06-30 01:51:55 -0400

    express generator

--- NodeJs/nodeExpress/index.js
+++ NodeJs/nodeExpress/index.js
@@ -4,6 +4,10 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
 const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+
+
 
 const hostname = "localhost";
 const port = 3000;
@@ -14,6 +18,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
 
 app.use(express.static(__dirname + "/public"));
 

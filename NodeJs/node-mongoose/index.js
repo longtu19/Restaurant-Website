f45e9322455a127dffb540b379ae39886c37d230
Long Huynh <longbhuynh019@gmail.com>
2022-07-03 21:09:35 -0400

    mongoose part 1

---
+++ NodeJs/node-mongoose/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion'
+
+const connect = mongoose.connect(url, {
+    useMongoClient: true
+})
+
+connect.then((db) => {
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save().then((dish) => {
+        console.log(dish);
+
+        Dishes.find({}).exec()
+    })
+    .then((dishes) => {
+        console.log(dishes)
+
+        return Dishes.remove({})
+    })
+    .then(() => {
+        return mongoose.connection.close()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+})

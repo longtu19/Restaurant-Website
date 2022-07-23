36cb76bc8b5232cbac217c7c01226e09fc4367a6
Long Huynh <longbhuynh019@gmail.com>
2022-07-22 21:00:15 -0400

    file upload

--- NodeJs/conFusionServer/app.js
+++ NodeJs/conFusionServer/app.js
@@ -7,18 +7,19 @@ var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 var passport = require("passport");
 var authenticate = require("./authenticate");
-var config = require('./config')
+var config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var uploadRouter = require("./routes/uploadRouter");
 
 const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
 
-const url = config.mongoUrl
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then(
@@ -31,14 +32,16 @@ connect.then(
 );
 
 var app = express();
-app.all('*', (req, res, next) => {
-  if (req.secure){
-    return next()
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      "https://" + req.hostname + ":" + app.get("secPort") + req.url
+    );
   }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url)
-  }
-})
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -49,19 +52,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-09876-54321"));
 
-
 app.use(passport.initialize());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
